8e715c88d68b0ecca57918cf0616cd5c5b450968
vvduniverse <86676363+vvduniverse@users.noreply.github.com>
2021-07-07 12:14:10 +0300

    graphic ready with round buttons

--- hw2.2_trafficLight/ViewController.swift
+++ hw2.2_trafficLight/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redColorLight: UIButton!
+    
+    @IBOutlet weak var yellowColorLight: UIButton!
+    
+    
+    @IBOutlet weak var greenColorLight: UIButton!
+    
+    
+    @IBOutlet weak var startNextButtonProperty: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorLight.alpha = 0.3
+        redColorLight.layer.cornerRadius = 65
+        yellowColorLight.alpha = 0.3
+        yellowColorLight.layer.cornerRadius = 65
+        greenColorLight.alpha = 0.3
+        greenColorLight.layer.cornerRadius = 65
+        startNextButtonProperty.layer.cornerRadius = 15
     }
 
-
+    @IBAction func startNextButtonPressed() {
+        startNextButtonProperty.setTitle("NEXT", for: .normal)
+    }
+    
 }
 
